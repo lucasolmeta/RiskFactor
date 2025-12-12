6e48723479eb2599f600f010dbe565ec0c9590d5
leolmeta <lolmetab@gmail.com>
2025-12-11 22:46:11 -0600

    debugged and added basic main() structure

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "RiskFactor.hpp"
+
 int main(){
-    
+    OddsEngine engine;
+
+    std::vector<std::string> odds = {"-300","+150","-100","+1900"};
+
+    std::vector<double> probs = engine.oddsToProb( odds );
+
+    for(int i = 0; i < probs.size(); i++){
+        std::cout << odds[i] << " is equal to " << probs[i] * 100.0 << "%" << std::endl;
+    }
 }
